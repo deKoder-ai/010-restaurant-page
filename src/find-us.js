63fdc272ddc837efca24b9290dfc8f8af033641f
FR33FA11 <dekoder.ai@pm.me>
2025-01-01 03:13:28 +0700

    Added Find Us HTML & styling

---
+++ src/find-us.js
@@ -0,0 +1,11 @@
+const map = L.map('map').setView([13.787639335950448, 100.54766081996733], 16.5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const marker = L.marker([13.787639335950448, 100.54766081996733]).addTo(map);
+marker.bindPopup("<div id='map-popup'><h3><b>HIDEOUT</h3></b>Bar & Restaurant</div>").openPopup();
+
+export { map, marker}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
 import { greeting } from './greeting.js';
+import {map, marker} from './find-us.js';
 
-
+// map = map;
+// marker = marker;
 
 console.log(greeting);
 
@@ -12,3 +14,13 @@ console.log(greeting);
 // myIcon.src = Icon;
 // const element = document.getElementById('main');
 // element.appendChild(myIcon);
+
+// const map = L.map('map').setView([13.787639335950448, 100.54766081996733], 16.5);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+
+// const marker = L.marker([13.787639335950448, 100.54766081996733]).addTo(map);
+// marker.bindPopup("<div id='map-popup'><h3><b>HIDEOUT</h3></b>Bar & Restaurant</div>").openPopup();
